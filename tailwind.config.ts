81351b7f296b60d6595bfed8aebacd0e0419685d
aayo <aayo.software@gmail.com>
2024-07-06 05:10:29 -0400

    first commit 👀

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,72 +1,17 @@
 import type { Config } from "tailwindcss";
 
-function customColors(cssVar: any): any {
-  return ({
-    opacityVariable,
-    opacityValue,
-  }: {
-    opacityVariable: any;
-    opacityValue: any;
-  }) => {
-    if (opacityValue !== undefined) {
-      return `rgba(var(${cssVar}), ${opacityValue})`;
-    }
-    if (opacityVariable !== undefined) {
-      return `rgba(var(${cssVar}), var(${opacityVariable}, 1))`;
-    }
-    return `rgb(var(${cssVar}))`;
-  };
-}
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
   theme: {
     extend: {
-      colors: {
-        primary: {
-          50: customColors("--c-primary-50"),
-          100: customColors("--c-primary-100"),
-          200: customColors("--c-primary-200"),
-          300: customColors("--c-primary-300"),
-          400: customColors("--c-primary-400"),
-          500: customColors("--c-primary-500"),
-          600: customColors("--c-primary-600"),
-          700: customColors("--c-primary-700"),
-          800: customColors("--c-primary-800"),
-          900: customColors("--c-primary-900"),
-        },
-        secondary: {
-          50: customColors("--c-secondary-50"),
-          100: customColors("--c-secondary-100"),
-          200: customColors("--c-secondary-200"),
-          300: customColors("--c-secondary-300"),
-          400: customColors("--c-secondary-400"),
-          500: customColors("--c-secondary-500"),
-          600: customColors("--c-secondary-600"),
-          700: customColors("--c-secondary-700"),
-          800: customColors("--c-secondary-800"),
-          900: customColors("--c-secondary-900"),
-        },
-        tertiary: {
-          50: customColors("--c-neutral-50"),
-          100: customColors("--c-neutral-100"),
-          200: customColors("--c-neutral-200"),
-          300: customColors("--c-neutral-300"),
-          400: customColors("--c-neutral-400"),
-          500: customColors("--c-neutral-500"),
-          600: customColors("--c-neutral-600"),
-          700: customColors("--c-neutral-700"),
-          800: customColors("--c-neutral-800"),
-          900: customColors("--c-neutral-900"),
-        },
-      },
       backgroundImage: {
-        hero: "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7)), url('/Hero/img-1-105a64ad.jpg')",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
